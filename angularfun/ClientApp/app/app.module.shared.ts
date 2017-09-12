c6367c8137563f0e98f03a38a1389fbef0d6bb5a
Krzysztof Sliwinski <krzysztof.sliwinski@assecods.pl>
2017-09-12 21:57:56 +0200

    modules

--- angularfun/ClientApp/app/app.module.shared.ts
+++ angularfun/ClientApp/app/app.module.shared.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
+import { HeroDetailComponent } from './components/app/hero-detail.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
@@ -16,7 +17,8 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+       HeroDetailComponent
     ],
     imports: [
         CommonModule,
--- angularfun/ClientApp/app/components/app/app.component.ts
+++ angularfun/ClientApp/app/components/app/app.component.ts
@@ -19,33 +19,17 @@ export class Hero {
 
 @Component({
     selector: 'app',
-   // templateUrl: './app.component.html',
-   template: `
-   <h1>{{title}}</h1>
-   <h2>{{hero.name}} details!</h2>
-   <div><label>id: </label>{{hero.id}}</div>
-   <div><label>name: </label>{{hero.name}}</div>
-   <div>
-   <label>name: </label>
-   <input [(ngModel)]="hero.name" placeholder="name">
-   <h2>My Heroes</h2>
-   <ul class="heroes">
-   <li *ngFor="let hero of heroes">
-   <span class="badge">{{hero.id}}</span> {{hero.name}}
- </li>
-   </ul>
- </div>
-   `,
+   templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
 
 
 export class AppComponent {
     title = "Tour of Heroes";
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-      };
+    selectedHero: Hero;
       heroes = HEROES;
+      onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+      }
    
 }
