bc34e0745b29cd0c35b1309b64f7b69cd7cd7f00
christophersliw <christophersliw@gmail.com>
2017-11-28 21:22:49 +0100

    rozne

--- helloword/Program.cs
+++ helloword/Program.cs
@@ -6,7 +6,20 @@ namespace helloword
     {
         static void Main(string[] args)
         {
+          //  B tmp = new B();
             Console.WriteLine("Hello World!");
         }
+    }
+
+
+    class A{
+        public A(int i)
+        {
+
+        }
+    }
+    class B : A{
+            public B(int i):base(i)
+            {}
     }
 }
