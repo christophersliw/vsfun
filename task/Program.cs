4df577ccd1efbb0e14963e2856f308bd3a6a3849
christophersliw <christophersliw@gmail.com>
2019-03-16 22:15:58 +0100

    'a'

---
+++ task/Program.cs
@@ -0,0 +1,46 @@
+﻿//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Threading;
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Method1();  
+             Console.WriteLine(" aaaa - " + Thread.CurrentThread.ManagedThreadId);  
+            Method2();  
+     
+        }
+
+        public static async Task Method1()  
+        {  
+            await Task.Run(() =>  
+            {  
+                for (int i = 0; i < 100; i++)  
+                {  
+                    Console.WriteLine(" Method 1  - " + Thread.CurrentThread.ManagedThreadId);  
+                }  
+            });  
+
+             Console.WriteLine(" ccccccccccccccccccccccccccccccccccccc");             
+        }  
+
+    
+        public static void Method2()  
+                {  
+                    for (int i = 0; i < 25; i++)  
+                    {  
+                        Console.WriteLine(" Method 2 - " + Thread.CurrentThread.ManagedThreadId);  
+                    }  
+                }  
+}   
+
+
+}
---
+++ webTest/Pages/Contact.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webTest.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your contact page xxxxxxxxxxxxx.";
+
+            new Repository().AddModel(1);
+        }
+    }
+
+    public class Repository
+    {
+        public  void AddModel(int id)
+        {
+            new ElasticService().IndexModel(1);
+
+            new ElasticService().IndexModelAndReturn(1);
+        }
+
+         public async  Task<int> AddModelAndReturn(int id)
+        {
+          int result = await new ElasticService().IndexModelAndReturn(1);
+
+          return 1;
+        }
+    } 
+
+    public class ElasticService
+    {
+        public  async void IndexModel(int id)
+        {
+            await Task.Run(() =>  
+            { 
+                for (int i = 0; i < 1000000000; i++)  
+                {  
+                    for (int j = 0; j < 1000000000; j++)  
+                {  
+                  
+                }  
+                }  
+            });      
+        }
+
+         public  async Task<int> IndexModelAndReturn(int id)
+        {
+            await Task.Run(() =>  
+            { 
+                for (int i = 0; i < 1000000000; i++)  
+                {  
+                    for (int j = 0; j < 1000000000; j++)  
+                {  
+                  
+                }  
+                }  
+            });      
+
+            return 1;
+        }
+    }
+}
